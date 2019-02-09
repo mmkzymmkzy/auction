2b484db02b906ff7ea724fb3c8b3480d53ebba8c
mmkzymmkzy <mmkzymmkzy@gmail.com>
2019-02-09 16:37:56 +0900

    登録機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,31 @@
 class ItemsController < ApplicationController
   def show
-  # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
-  # 取得したデータを@itemというインスタンス変数にセットする
-  @item = Item.find(params[:id])
+    # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
+    # 取得したデータを@itemというインスタンス変数にセットする
+    @item = Item.find(params[:id])
+  end
+  def index
+    @items = Item.all
   end
 
-  def index
-  @items = Item.all
+  def new
+    @item = Item.new
+  end
+  
+  def create 
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item 
+  end
+
+  private
+
+  def item_params
+    parrams.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
   end
 end
+
+
 
 
 
