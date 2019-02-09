100e752f63c5ba4f51228adaba20746797ee3ce5
mmkzymmkzy <mmkzymmkzy@gmail.com>
2019-02-09 17:50:33 +0900

    編集削除完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
   def show
-    # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
-    # 取得したデータを@itemというインスタンス変数にセットする
     @item = Item.find(params[:id])
   end
+
   def index
     @items = Item.all
   end
@@ -11,21 +10,41 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
-  def create 
+
+  def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to @item 
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
   end
 
   private
 
   def item_params
-    parrams.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+    params.require(:item).permit(:name,
+                                 :price,
+                                 :seller,
+                                 :description,
+                                 :email,
+                                 :image_url)
   end
+
 end
-
-
 
 
 
